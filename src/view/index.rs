6cb2d261593a0a243d4fb075ceed4ad4d5fd73dd
tangjz <36634584@qq.com>
2025-02-26 11:22:15 +0800

    feat: add fakemail

--- src/view/index.rs
+++ src/view/index.rs
@@ -322,6 +322,16 @@ pub fn Index() -> impl IntoView {
                         </div>
                     </div>
                 </a>
+                <a href="https://fakemail.work/" target="_blank">
+                    <div
+                        class="flex items-center h-20 px-3 lg:px-5 bg-white hover:translate-y-[-1px] hover:shadow-lg transition-transform duration-300">
+                        <img class="w-10 h-10" src="/images/fakemail.work.png" alt="" />
+                        <div class="ml-3 truncate">
+                            <h3 class="text-sm truncate">FakeMail</h3>
+                            <p class="text-xs text-gray-400 truncate">安全且匿名的临时邮件服务</p>
+                        </div>
+                    </div>
+                </a>
             </div>
         </section>
         <section class="my-5 bg-white shadow-md p-6">
